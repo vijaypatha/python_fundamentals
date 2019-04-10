8cce92390d0afab4a97e70fb6528718e1f06012c
Patha <vipatha@amazon.com>
2019-04-09 19:54:10 -0700

    functions

--- function.py
+++ function.py
@@ -24,3 +24,9 @@ def is_short(name):
 short_cities = list(filter(is_short, cities))
 print(short_cities)
 
+# Using lamda function
+
+cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
+
+short_cities = list(filter(lambda x: len(x) < 10, cities))
+print(short_cities)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,6 @@
+# Using lamda function
+
 cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
 
-def is_short(name):
-    return len(name) < 10
-print(name)
+short_cities = list(filter(lambda x: len(x) < 10, cities))
+print(short_cities)
